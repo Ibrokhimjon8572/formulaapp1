6b34742db9b277631df0e8ae274d08686e1c1f1c
Mamajonov Ibrohimjon <mamajonovibrokhimjon@gmail.com>
2023-10-20 17:57:37 +0500

    t3 commit

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from chempionship import Chempionship
+from driver import Driver
+from timeform import TimeForm
+from gp import GP
 
-print('test')
 chempionship = Chempionship()
 
-print(chempionship.create_driver('ali'))
-print(chempionship.create_driver('vali'))
-print(chempionship.get_drivers())
-print(chempionship.get_driver('ali'))
+gp = GP('mers form')
+driver = Driver('ali')
+end_time = TimeForm(12, 45, 34, 123)
+print(chempionship.set_time(gp, driver, end_time))
+
+# print(chempionship.define_gp('ford form'))
+# print(chempionship.get_gp('ford form'))
+# print(chempionship.create_driver('ali'))
+# print(chempionship.create_driver('vali'))
+# print(chempionship.get_drivers())
+# print(chempionship.get_driver('ali'))
---
+++ time_gp.py
@@ -0,0 +1,13 @@
+from driver import Driver
+from gp import GP
+from timeform import TimeForm
+
+class Time:
+    def __init__(self, gp: GP, driver: Driver, tugash_vaqti: TimeForm) -> None:
+        self._gp = gp
+        self._driver = driver
+        self._tugash_vaqti = tugash_vaqti
+    
+    def __str__(self) -> str:
+        a = self._tugash_vaqti
+        return a.to_string()
